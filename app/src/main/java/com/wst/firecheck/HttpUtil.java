afa9c60efcfb4b5c2d29725235a399465fc5b674
Alex_li <li44979@hotmail.com>
2018-01-25 17:17:51 +0800

    http调用类
    调用类及示范例

---
+++ app/src/main/java/com/wst/firecheck/HttpUtil.java
@@ -0,0 +1,19 @@
+package com.wst.firecheck;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by li449 on 2018/1/25.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(final String url,final Callback callback)
+    {
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+}
--- app/src/main/java/com/wst/firecheck/MainActivity.java
+++ app/src/main/java/com/wst/firecheck/MainActivity.java
@@ -1,61 +1,61 @@
 package com.wst.firecheck;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import okhttp3.Callback;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    Button me;
     private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<String> names= Arrays.asList("peter","anna","mike");
-        Collections.sort(names,(String a,String b)->{
-            int i=b.compareTo(a);
-            Log.e(TAG, "ll---l"+i );
-            return i;
-        });
-        new Thread(){
+        tv=findViewById(R.id.tvapi);
+        me=findViewById(R.id.me);
+        me.setOnClickListener((v)-> {
+            Toast.makeText(MainActivity.this, "你点击了我", Toast.LENGTH_LONG).show();
+        } );
+        String url="http://192.168.0.103:8080/api/values/5";
+        new Thread(new Runnable() {
             @Override
-            public void run()
-            {
-                String result= Run("http://10.0.2.2:54331/test/get");
-                Log.d(TAG, "返回结果"+result);
-                handler.sendEmptyMessage(0);
-            }
-            private Handler handler=new Handler(){
-                @Override
-                public void handleMessage(Message msg)
-                {
-                    super.handleMessage(msg);
-                }
-            };
+            public void run() {
+                HttpUtil.sendOkHttpRequest(url, new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
 
-        }.start();
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        String responseData=response.body().string();
+                        showResponse(responseData);
+                    }
+                });
+            }
+        }).start();
     }
-    String Run(String url){
-        OkHttpClient client=new OkHttpClient();
-        try{
-             Request request=new Request.Builder()
-                                .url(url)
-                                .build();
-             Call call=client.newCall(request);
-             Response response=call.execute();
-             return  response.body().string();
-        }catch (IOException ieo){
-            ieo.printStackTrace();
-        }
-        return "";
-    }
+          private void showResponse(final String response)
+          {
+              runOnUiThread(()-> {
+                    Log.d(TAG, "~"+response+"~");
+                      List<User> userList= JSON.parseArray(response,User.class);//反序列化
+                     for(User u:userList){
+                         Log.d(TAG, Integer.toString(u.getId()));
+                         Log.d(TAG, u.getName());
+                     }
+              });
+          }
 }
